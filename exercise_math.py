3ff9ddc56a6eb1d7fd4e68a88293c1c24eac5514
Larro123 <slarrosa@mail.austral.edu.ar>
2025-08-11 12:55:16 -0300

    Add files via upload

--- exercise_math.py
+++ exercise_math.py
@@ -1,3 +1,18 @@
 def math():
     a = 57
     b = 7
+    suma = a + b
+    print(suma)
+    resta = a - b
+    print(resta)
+    producto = a * b
+    print(producto)
+    promedio = (a + b)/ 2
+    print(promedio)
+    division = a / b
+    print(int(division))
+    resto =  a%b
+    print(resto)
+    division_f = a / b
+    print(float(division_f))
+#math() 
